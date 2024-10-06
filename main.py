f18bf9b34e57c8ad65ad5681511e082cc2091d0d
Kemran <morpeh2005ka@gmail.com>
2024-10-06 12:21:44 +0300

    Сделал датасет

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
+import csv
+
+# спарсим данные с csv файла
+def load_dataset() -> list:
+    res = []
+    with open("usgs_japan_1968_2018.csv", "r") as f:
+        for row in csv.reader(f,delimiter=','):
+            try:
+                # будем добавлять только широту, долготу и магнитуду
+                res.append([float(row[1]),float(row[2]),float(row[4])])
+            except:
+                continue
+    return res
+        
 def main():
-    print()
+    data = load_dataset()
 
 if __name__ == "__main__":
     main()
